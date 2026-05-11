1ab8492150439d827e9c9b8ddcd7926381d81101
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-05-11 16:03:26 +0700

    Port Setup

--- server/index.js
+++ server/index.js
@@ -52,3 +52,21 @@ app.get('/values/current', async (req, res) => {
     res.send(values);
   });
 });
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log('Listening');
+});
