c50b564fec839480af82421bb4048a037cb77609
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-05-11 16:02:31 +0700

    Express Route Config

--- server/index.js
+++ server/index.js
@@ -35,3 +35,20 @@ const redisClient = redis.createClient({
   retry_strategy: () => 1000,
 });
 const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
