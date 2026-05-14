c674a56ed9f358333ee77eb48d8f513650d66107
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-05-14 15:15:07 +0700

    Update Keys entry for Server

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,11 @@
 module.exports = {
+  // Redis - Support Both REDIS_URL (cloud) and REDIS_HOST+PORT (local Docker)
+  redisUrl: process.env.REDIS_URL,
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
+
+  // PostgreSQL - Support Both DATABASE_URL (cloud) and for each difference variable (local Docker)
+  databaseUrl: process.env.DATABASE_URL,
   pgUser: process.env.PGUSER,
   pgHost: process.env.PGHOST,
   pgDatabase: process.env.PGDATABASE,
