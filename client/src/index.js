bb52397902659e057bfa29fa75419dd5a0e35e67
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-05-14 15:07:06 +0700

    Fix: restore index.js entry point

--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import OtherPage from "./OtherPage";
-import Fib from "./Fib";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <Routes>
-          <Route path="/" element={<Fib />} />
-          <Route path="/otherpage" element={<OtherPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-export default App;
-
-
+root.render(<App />);
